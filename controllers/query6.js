98735b0ef5798f2019a82836876eafc26bf84e7b
Hai Duong <149478749+HaixD@users.noreply.github.com>
2025-06-05 19:04:50 -0700

    fix query 6

--- controllers/query6.js
+++ controllers/query6.js
@@ -9,9 +9,9 @@ const addGame = async (req, res) => {
         await client.query("BEGIN");
         await client.query(`ALTER TYPE GENRE_NAME ADD VALUE IF NOT EXISTS '${genre}'`);
         await client.query("COMMIT");
-        const matchingGenres = await client.query('SELECT "ID" FROM genre WHERE "Genre"=$1', [genre]);
+        let matchingGenres = await client.query('SELECT "ID" FROM genre WHERE "Genre"=$1', [genre]);
         if (matchingGenres.rowCount === 0) {
-            await client.query('INSERT INTO genre ("Genre") VALUES ($1)', [genre]);
+            matchingGenres = await client.query('INSERT INTO genre ("Genre") VALUES ($1) RETURNING *', [genre]);
         }
         const result = await client.query('INSERT INTO Game ("Name", "Genre", "Preview", "Description", "Link") VALUES ($1, $2, NULL, $3, $4) RETURNING *', [name, matchingGenres.rows[0].ID, description, link]);
         await client.query("COMMIT");
